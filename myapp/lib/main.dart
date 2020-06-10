3493314124bd29a75f9c4e3e6224e520add5126e
Carlos Hwa <carloshwa@gmail.com>
2020-06-10 10:42:29 -0700

    Add to local history

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -53,6 +53,12 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    ModalRoute.of(context).addLocalHistoryEntry(LocalHistoryEntry(onRemove: () {
+      setState(() {
+        _counter--;
+      });
+    }));
+
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
